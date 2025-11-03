a0c4f2764fcd722acf0c3170a2773e862c21a718
Mahesh8120 <jonnalagadamahi@gmail.com>
2025-11-03 18:15:31 +0530

    main

--- outputs.tf
+++ outputs.tf
@@ -17,17 +17,13 @@ output "database_rt" {
   value = aws_route_table.database.id
 }
 
-output "vpc_id" {
-    value = module.vpc.vpc_id
-}
+
 
 # output "azs" {
 #     value = data.aws_availability_zones.available
 # }
 
-output "public_subnet_ids" {
-    value = module.vpc.public_subnet_ids
-}
+
 output "vpc_id" {
   value       = aws_vpc.main.id
 }
